341d694423f8e0b8f31db3ac946f5ef13cd9f969
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-03-16 19:06:29 +0600

    style jsx

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+const names = ' SaJeeb AHmeed ';
+const age = 25;
+const obi = {
+  name: 'saju',
+  job: 'web developer',
+
+}
+const styleadd = {
+  color: 'red',
+  backgroundColor: 'yellow'
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h2>JSX</h2>
+        <div className="container">
+          <p style={styleadd}>{names} age {20 + age}</p>
+          <p style={{ color: 'blue', backgroundColor: 'green', padding: '20px' }}> {obi.name} {obi.job}</p>
+          <p> {obi.name} {obi.job}</p>
+        </div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          cEdit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
